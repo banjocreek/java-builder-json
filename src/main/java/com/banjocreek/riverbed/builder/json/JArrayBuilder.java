5cf35e5613bbe42bed25680918e331d8fcc2efca
Greg Wiley <aztec.rex@jammm.com>
2015-02-10 13:45:16 -0800

    [doc] javadoc object builder

--- src/main/java/com/banjocreek/riverbed/builder/json/JArrayBuilder.java
+++ src/main/java/com/banjocreek/riverbed/builder/json/JArrayBuilder.java
@@ -23,28 +23,122 @@ import java.math.BigInteger;
 import javax.json.JsonArray;
 import javax.json.JsonValue;
 
+/**
+ * <p>
+ * JSON Array builder interface. Defines the common builder operations for
+ * arrays. The mutable,
+ * {@link com.banjocreek.riverbed.builder.json.mutable.JAry JAry}, and immutable
+ * {@link com.banjocreek.riverbed.builder.json.immutable.JAry JAry}
+ * sub-interfaces define nesting, merging, and building.
+ * </p>
+ *
+ * @param <Z>
+ */
 public interface JArrayBuilder<Z extends JArrayBuilder<Z>> {
 
+    /**
+     * Add a value to the end of the array.
+     *
+     * @param value
+     *            value to add.
+     *
+     * @return mutated builder
+     */
     public Z add(final BigDecimal value);
 
+    /**
+     * Add a value to the end of the array.
+     *
+     * @param value
+     *            value to add.
+     *
+     * @return mutated builder
+     */
     public Z add(final BigInteger value);
 
+    /**
+     * Add a value to the end of the array.
+     *
+     * @param value
+     *            value to add.
+     *
+     * @return mutated builder
+     */
     public Z add(final boolean value);
 
+    /**
+     * Add a value to the end of the array.
+     *
+     * @param value
+     *            value to add.
+     *
+     * @return mutated builder
+     */
     public Z add(final double value);
 
+    /**
+     * Add a value to the end of the array.
+     *
+     * @param value
+     *            value to add.
+     *
+     * @return mutated builder
+     */
     public Z add(final int value);
 
+    /**
+     * Add a value to the end of the array.
+     *
+     * @param value
+     *            value to add.
+     *
+     * @return mutated builder
+     */
     public Z add(final JsonValue value);
 
+    /**
+     * Add a value to the end of the array.
+     *
+     * @param value
+     *            value to add.
+     *
+     * @return mutated builder
+     */
     public Z add(final long value);
 
+    /**
+     * Add a value to the end of the array.
+     *
+     * @param value
+     *            value to add.
+     *
+     * @return mutated builder
+     */
     public Z add(final String value);
 
+    /**
+     * Add a JSON null value to the end of the array.
+     *
+     * @return mutated builder
+     */
     public Z addNull();
 
+    /**
+     * Empty the builder. The result of this operation is a builder that will
+     * create an empty array.
+     *
+     * @return mutated builder
+     */
     public Z clear();
 
+    /**
+     * Append elements from an existing array.
+     *
+     * @param jary
+     *            array with elements to add.
+     *
+     * @return mutated builder
+     */
     public Z concat(JsonArray jary);
 
 }
